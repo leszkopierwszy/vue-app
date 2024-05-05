46c6ebf11070fb8dd84e43649c563ac9f1557c87
Leczero <leszko.bartosz@gmail.com>
2024-05-05 20:12:50 +0200

    simple header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import headerComponent from './components/header-component.vue';
+
+const app = createApp({});
+app.component('header-component', headerComponent)
+
+app.mount("#app");
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,12 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div id="app">
+        <!--  -->
+        <header-component/>
+    </div>
 </body>
 </html>
